3df4ef8d721e8bd2629b7ba74bdab137544ce31d
timon w. mesulam <timon.w.mesulam935xpacenuchra@gmail.com>
2022-03-27 15:56:44 +0300

    commit ..8

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,14 @@
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
+pkg install curl
+curl "https://assorted.downloads.oppo.com/firmware/CPH2179/CPH2179PU_11_OTA_0210_all_6LDsHsSzX62H.ozip"
+echo
+echo
+echo "************************************"
+
+ls
+echo
+echo
+echo "------------------------------------"
+pwd
